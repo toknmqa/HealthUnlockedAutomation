e1c73ccdc75db3b84e58808bb7e78a514d7ed0d5
rjanjua <rjanjua@qaworks.com>
2014-05-19 17:46:30 +0100

    test 2

--- src/test/java/PageObjects/Community.java
+++ src/test/java/PageObjects/Community.java
@@ -24,7 +24,7 @@ public class Community extends WebPageHelpers {
 
 
 
-	public void openPostsQuestions() { //Another test comment to check from Github mac app
+	public void openPostsQuestions() { //Another test comment
 
 		communityURL = getDriver().getCurrentUrl().toString();
 		communityURL = communityURL + "/posts";
