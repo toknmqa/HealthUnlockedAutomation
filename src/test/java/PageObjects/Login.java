5294b837125fa3c14fdb79b3f90e66290d21f01c
Panawat Iteeyaporn <piteey01@gmail.com>
2014-05-20 11:40:05 +0100

    Unused imports removed.

--- src/test/java/PageObjects/Login.java
+++ src/test/java/PageObjects/Login.java
@@ -1,29 +1,10 @@
 package PageObjects;
 
-import java.io.File;
+
 import static org.junit.Assert.assertTrue;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-
-import helpers.SeleniumFactory;
 import helpers.WebPageHelpers;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Login extends WebPageHelpers {
