7d95e2b500ec79196bb685a14f80a7fb5e2d20cf
rjanjua <rjanjua@qaworks.com>
2014-05-21 09:14:55 +0100

    Created an address class for storing static addresses

---
+++ src/test/java/helpers/Address.java
@@ -0,0 +1,6 @@
+package helpers;
+
+public final class Address {
+	public static final String HOMEPAGE = "https://bg-test.www.ppgdes.com";
+	public static final String DIRECTORYPAGE = HOMEPAGE + "/directory";
+}
